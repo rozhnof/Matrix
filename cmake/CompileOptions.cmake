ebc4ee6ab068e53780050e458f4f20ac8e2d0e13
Yesenia Sunshine <yesenias@in-e4.nsk.21-school.ru>
2024-06-09 20:13:01 +0700

    build: fix build errors

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -1,6 +1,5 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
 
 add_compile_options(-Wall -Wextra -Wpedantic -g -fno-omit-frame-pointer)
 add_compile_options(-Wno-language-extension-token)
